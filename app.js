2a5f714a665c644a0251687aa66e2a26c8158882
KyleHS <k15156@yahoo.com>
2023-06-20 18:37:19 -0600

    created a for loop that manipulates all chicken images into spamton >:)

--- app.js
+++ app.js
@@ -5,8 +5,14 @@ const allLinks = document.querySelectorAll('a');
 // }
 
 
-for (let link of allLinks) {
-    link.style.color = 'rgb(0, 108, 134)';
-    link.style.textDecorationColor = 'magenta';
-    link.style.textDecorationStyle = 'wavy'
-}
+//for (let link of allLinks) {
+//   link.style.color = 'rgb(0, 108, 134)';
+//   link.style.textDecorationColor = 'magenta';
+//  link.style.textDecorationStyle = 'wavy'
+// }
+
+const allImages= document.getElementsByTagName('img');  //this selects all of our img objects
+
+for (let img of allImages) {
+    img.src = 'https://cdn.shopify.com/s/files/1/0014/1962/products/product_DR_spamton_plush_main.png?v=1650408119'
+}  //this loop manipulates the page and replaces all silkie chicken images with spamton images :0
